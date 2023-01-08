565c446b27e63d7cbd6f22390c96ff04c0f61039
alvittoag <alvittogenz09@gmail.com>
2023-01-08 23:37:55 +0700

    Add index for component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ChannelDetail, Feed, SearchFeed, VideoDetail } from "./components";
 import { Box } from "@mui/system";
-import Feed from "./components/Feed";
-import VideoDetail from "./components/VideoDetail";
-import ChannelDetail from "./components/ChannelDetail";
-import SearchFeed from "./components/SearchFeed";
-import Navbar from "./components/Navbar";
 
-const App = () => {
-  <BrowserRouter>
-    <Box sx={{ backgroundColor: "#000" }}>Navbar</Box>
-    <Routes>
-      <Navbar />
-      <Route path="/" element={<Feed />} />
-      <Route path="/video/:id" element={<VideoDetail />} />
-      <Route path="/channel/:id" element={<ChannelDetail />} />
-      <Route path="/search/:searchTerm" element={<SearchFeed />} />
-    </Routes>
-  </BrowserRouter>;
-};
+function App() {
+  return (
+    <Router>
+      <Box sx={{ backgroundColor: "red" }}>
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/video/:id" element={<VideoDetail />} />
+          <Route path="/channel/:id" element={<ChannelDetail />} />
+          <Route path="/search/:searchTerm" element={<SearchFeed />} />
+        </Routes>
+      </Box>
+    </Router>
+  );
+}
 
 export default App;
--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  );
 };
 
 export default Feed;
